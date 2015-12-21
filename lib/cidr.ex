2936a407332992fbafc74f8da3b3a6e1068ac714
Constantin Rack <constantin@rack.li>
2015-12-21 10:17:17 +0100

    Problem: credo warnings
    Solution: fix them by refactoring

--- lib/cidr.ex
+++ lib/cidr.ex
@@ -40,12 +40,12 @@ defmodule CIDR do
   end
   def match(%CIDR{start: {a, b, c, d}, end: {e, f, g, h}}, address = {i, j, k, l}) do
     if is_ipv4(address) do
-      match =
+      result =
         i in a..e and
         j in b..f and
         k in c..g and
         l in d..h
-      {:ok, match}
+      {:ok, result}
     else
       {:error, "Tuple is not a valid IP address."}
     end
@@ -53,7 +53,7 @@ defmodule CIDR do
   def match(%CIDR{start: {a, b, c, d, e, f, g, h}, end: {i, j, k, l, m, n, o, p}},
             address = {q, r, s, t, u, v, w, x}) do
     if is_ipv6(address) do
-      match =
+      result =
         q in a..i and
         r in b..j and
         s in c..k and
@@ -62,7 +62,7 @@ defmodule CIDR do
         v in f..n and
         w in g..o and
         x in h..p
-      {:ok, match}
+      {:ok, result}
     else
       {:error, "Tuple is not a valid IP address."}
     end
@@ -75,7 +75,7 @@ defmodule CIDR do
   """
   def match!(cidr, address) do
     case match(cidr, address) do
-      {:ok,    match}  -> match
+      {:ok,    result} -> result
       {:error, reason} -> raise ArgumentError, message: reason
     end
   end
@@ -97,10 +97,10 @@ defmodule CIDR do
 
   # We got a simple IP address without mask
   defp parse(address, []) when tuple_size(address) == 4 do
-    create(address, address, 32, hosts(:ipv4, 32))
+    create(address, address, 32, num_hosts(:ipv4, 32))
   end
   defp parse(address, []) when tuple_size(address) == 8 do
-    create(address, address, 128, hosts(:ipv6, 128))
+    create(address, address, 128, num_hosts(:ipv6, 128))
   end
   # We got a mask and need to convert it to integer
   defp parse(address, [mask]) do
@@ -115,10 +115,15 @@ defmodule CIDR do
   end
   # Everything is fine
   defp parse(address, mask) when tuple_size(address) == 4 do
-    create(start_address(address, mask), end_address(address, mask), mask, hosts(:ipv4, mask))
+    parse(address, mask, :ipv4)
   end
   defp parse(address, mask) when tuple_size(address) == 8 do
-    create(start_address(address, mask), end_address(address, mask), mask, hosts(:ipv6, mask))
+    parse(address, mask, :ipv6)
+  end
+  defp parse(address, mask, version) do
+    first = start_address(address, mask)
+    last  = end_address(address, mask)
+    create(first, last, mask, num_hosts(version, mask))
   end
 
   defp parse_address(address) do
@@ -134,59 +139,67 @@ defmodule CIDR do
     }
   end
 
-  defp hosts(:ipv4, mask) do
+  defp num_hosts(:ipv4, mask) do
     1 <<< (32 - mask)
   end
-  defp hosts(:ipv6, mask) do
+  defp num_hosts(:ipv6, mask) do
     1 <<< (128 - mask)
   end
 
-  defp start_address({a, b, c, d}, mask) do
-    s   = (32 - mask)
-    x   = (((a <<< 24) ||| (b <<< 16) ||| (c <<< 8) ||| d) >>> s) <<< s
-    a1  = ((x >>> 24) &&& 0xFF)
-    b1  = ((x >>> 16) &&& 0xFF)
-    c1  = ((x >>>  8) &&& 0xFF)
-    d1  = ((x >>>  0) &&& 0xFF)
-    {a1, b1, c1, d1}
+  defp start_address({_, _, _, _} = tuple, mask) do
+    s = (32 - mask)
+    x = tuple2number(tuple, s)
+    a = ((x >>> 24) &&& 0xFF)
+    b = ((x >>> 16) &&& 0xFF)
+    c = ((x >>>  8) &&& 0xFF)
+    d = ((x >>>  0) &&& 0xFF)
+    {a, b, c, d}
   end
-  defp start_address({a, b, c, d, e, f, g, h}, mask) do
-    s   = (128 - mask)
-    x   = (((a <<< 112) ||| (b <<< 96) ||| (c <<< 80) ||| (d <<< 64) ||| (e <<< 48) ||| (f <<< 32) ||| (g <<< 16) ||| h) >>> s) <<< s
-    a1  = ((x >>> 112) &&& 0xFFFF)
-    b1  = ((x >>>  96) &&& 0xFFFF)
-    c1  = ((x >>>  80) &&& 0xFFFF)
-    d1  = ((x >>>  64) &&& 0xFFFF)
-    e1  = ((x >>>  48) &&& 0xFFFF)
-    f1  = ((x >>>  32) &&& 0xFFFF)
-    g1  = ((x >>>  16) &&& 0xFFFF)
-    h1  = ((x >>>   0) &&& 0xFFFF)
-    {a1, b1, c1, d1, e1, f1, g1, h1}
+  defp start_address({_, _, _, _, _, _, _, _} = tuple, mask) do
+    s = (128 - mask)
+    x = tuple2number(tuple, s)
+    a = ((x >>> 112) &&& 0xFFFF)
+    b = ((x >>>  96) &&& 0xFFFF)
+    c = ((x >>>  80) &&& 0xFFFF)
+    d = ((x >>>  64) &&& 0xFFFF)
+    e = ((x >>>  48) &&& 0xFFFF)
+    f = ((x >>>  32) &&& 0xFFFF)
+    g = ((x >>>  16) &&& 0xFFFF)
+    h = ((x >>>   0) &&& 0xFFFF)
+    {a, b, c, d, e, f, g, h}
   end
 
-  defp end_address({a, b, c, d}, mask) do
-    s   = (32 - mask)
-    x   = (((a <<< 24) ||| (b <<< 16) ||| (c <<< 8) ||| d) >>> s) <<< s
-    y   = x ||| ((1 <<< s) - 1)
-    a1  = ((y >>> 24) &&& 0xFF)
-    b1  = ((y >>> 16) &&& 0xFF)
-    c1  = ((y >>>  8) &&& 0xFF)
-    d1  = ((y >>>  0) &&& 0xFF)
-    {a1, b1, c1, d1}
+  defp end_address({_, _, _, _} = tuple, mask) do
+    s = (32 - mask)
+    x = tuple2number(tuple, s)
+    y = x ||| ((1 <<< s) - 1)
+    a = ((y >>> 24) &&& 0xFF)
+    b = ((y >>> 16) &&& 0xFF)
+    c = ((y >>>  8) &&& 0xFF)
+    d = ((y >>>  0) &&& 0xFF)
+    {a, b, c, d}
   end
-  defp end_address({a, b, c, d, e, f, g, h}, mask) do
-    s   = (128 - mask)
-    x   = (((a <<< 112) ||| (b <<< 96) ||| (c <<< 80) ||| (d <<< 64) ||| (e <<< 48) ||| (f <<< 32) ||| (g <<< 16) ||| h) >>> s) <<< s
-    y   = x ||| ((1 <<< s) - 1)
-    a1  = ((y >>> 112) &&& 0xFFFF)
-    b1  = ((y >>>  96) &&& 0xFFFF)
-    c1  = ((y >>>  80) &&& 0xFFFF)
-    d1  = ((y >>>  64) &&& 0xFFFF)
-    e1  = ((y >>>  48) &&& 0xFFFF)
-    f1  = ((y >>>  32) &&& 0xFFFF)
-    g1  = ((y >>>  16) &&& 0xFFFF)
-    h1  = ((y >>>   0) &&& 0xFFFF)
-    {a1, b1, c1, d1, e1, f1, g1, h1}
+  defp end_address({_, _, _, _, _, _, _, _} = tuple, mask) do
+    s = (128 - mask)
+    x = tuple2number(tuple, s)
+    y = x ||| ((1 <<< s) - 1)
+    a = ((y >>> 112) &&& 0xFFFF)
+    b = ((y >>>  96) &&& 0xFFFF)
+    c = ((y >>>  80) &&& 0xFFFF)
+    d = ((y >>>  64) &&& 0xFFFF)
+    e = ((y >>>  48) &&& 0xFFFF)
+    f = ((y >>>  32) &&& 0xFFFF)
+    g = ((y >>>  16) &&& 0xFFFF)
+    h = ((y >>>   0) &&& 0xFFFF)
+    {a, b, c, d, e, f, g, h}
+  end
+
+  defp tuple2number({a, b, c, d}, s) do
+    (((a <<< 24) ||| (b <<< 16) ||| (c <<< 8) ||| d) >>> s) <<< s
+  end
+  defp tuple2number({a, b, c, d, e, f, g, h}, s) do
+    (((a <<< 112) ||| (b <<< 96) ||| (c <<< 80) ||| (d <<< 64)
+    ||| (e <<< 48) ||| (f <<< 32) ||| (g <<< 16) ||| h) >>> s) <<< s
   end
 
   defp is_ipv6(address) when tuple_size(address) == 8 do
